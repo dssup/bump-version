8222014d844bac85eae346839c4423b5fc055b36
Daniil Stepanov <dstepanov485@gmail.com>
2025-09-16 08:52:15 +0300

    refactor(askYesNo): add blank lines around code blocks

--- utils.go
+++ utils.go
@@ -34,21 +34,25 @@ func writeJSONFile(name string, v any, perm os.FileMode) error {
 // It accepts "y", "yes", "n", "no" (any case). If the reader hits EOF, it returns the provided defaultVal.
 func askYesNo(r io.Reader, w io.Writer, question string, defaultVal bool) bool {
 	reader := bufio.NewReader(r)
+
 	for {
 		defaultPrompt := "y/N"
 		if defaultVal {
 			defaultPrompt = "Y/n"
 		}
+
 		fmt.Fprintf(w, "%s [%s]: ", question, defaultPrompt)
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			// on EOF or other read error, return default
 			return defaultVal
 		}
+
 		response := strings.TrimSpace(strings.ToLower(line))
 		if response == "" {
 			return defaultVal
 		}
+
 		switch response {
 		case "y", "yes":
 			return true
